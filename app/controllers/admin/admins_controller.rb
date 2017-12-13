3f6aac79717c8ea6f391084f4ac8854fcb01de1f
Andrey Aksenov <tombery1@ya.ru>
2017-12-13 22:26:52 +0300

    добавил юзера и админа

---
+++ app/controllers/admin/admins_controller.rb
@@ -0,0 +1,6 @@
+class Admin::AdminsController < ActionController::Base
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+ 
+  root to: "articles#index"
+
   resources :articles
+
+  devise_for :users
+
+  namespace :admin do
+    devise_for :admins
+  end
 end
